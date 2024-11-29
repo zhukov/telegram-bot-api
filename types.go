65828338a94286d1027fa0de9bcd19ce29c0e12a
Igor Zhukov <igor@zhukov.io>
2024-11-30 00:13:58 +0100

    Update types.go

--- types.go
+++ types.go
@@ -5018,6 +5018,8 @@ type SuccessfulPayment struct {
 	TelegramPaymentChargeID string `json:"telegram_payment_charge_id"`
 	// ProviderPaymentChargeID provider payment identifier
 	ProviderPaymentChargeID string `json:"provider_payment_charge_id"`
+
+	SubscriptionExpirationDate int `json:"subscription_expiration_date"`
 }
 
 // RefundPayment contains basic information about a refunded payment.
